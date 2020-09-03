d8e9e780189609b851a61c555ded7a6e1ac64ecc
Kingsley Eoka <kingsleyenoka@gmail.com>
2020-09-03 12:38:04 -0500

    Save changes

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [value, onChangeText] = useState('');
+  const [valueTwo, onChangeTextTwo] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.textInputStyle}
+        placeholder = 'Enter Username'
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />
+      <TextInput
+        style={styles.textInputStyle}
+        placeholder = 'Enter Password'
+        onChangeText={text => onChangeTextTwo(text)}
+        value={valueTwo}
+      />
+      <Button 
+        title = 'Login'
+        onPress = {()=> {
+          onChangeText('')
+          onChangeTextTwo('')
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +40,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInputStyle: {
+    height: 40,
+    width: 200,
+    textAlign: 'center'
+  }
 });
